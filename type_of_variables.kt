ce6f1b0463849d27350ea3089683d7edcb0f0817
Yash12007 <106073201+Yash12007@users.noreply.github.com>
2022-10-17 19:41:47 +0530

    Create type_of_variables.kt

---
+++ type_of_variables.kt
@@ -0,0 +1,5 @@
+val myNum = 8             // Int
+val myDoubleNum = 8.99    // Double
+val myLetter = 'A'        // Char
+val myBoolean = true      // Boolean
+val myText = "Hello World"      // String
